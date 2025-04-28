62bd206a4e42a167b345ebb482e555436099f3b3
MrNuthikatla <abhinavabhi793@gmail.com>
2025-04-28 09:17:20 +0530

    Updatedn UserController

--- src/main/java/com/fsad/auction_backend/controller/UserController.java
+++ src/main/java/com/fsad/auction_backend/controller/UserController.java
@@ -1,14 +1,16 @@
 package com.fsad.auction_backend.controller;
 
 import com.fsad.auction_backend.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// This is just a outlier need to be developed completely
 @RestController
 @RequestMapping("/users")
-public class UserController {
+class UserController {
 
     @PostMapping("/register")
     public String registerUser(@RequestBody User user) {
@@ -16,5 +18,18 @@ public class UserController {
             return "All fields are required.";
         }
         return "User " + user.getUsername() + " registered with role: " + user.getRole();
+    }
+
+    @GetMapping("/login")
+    public String loginUser(@RequestBody User user) {
+        if (user.getUsername() == null || user.getPassword() == null) {
+            return "Username and password are required.";
+        }
+        return "User " + user.getUsername() + " logged in successfully.";
+    }
+
+    @GetMapping("/logout")
+    public String logoutUser() {
+        return "User logged out successfully.";
     }
 }
